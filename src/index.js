fe208ae2cd9cd6bcc267f2438b155b8c484e2cda
nielvag <nielsonvagno@gmail.com>
2019-08-18 14:16:24 -0300

    Profile, env vars  e config. da porta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -6,7 +9,12 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const mongoose = require('./db_config');
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.use((req, res, next) => {
 	req.io = io;
@@ -18,6 +26,6 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
 	console.log(':) Server started on port 3000');
 });
